9664c5a72de17d7fcb78562ec2d020471a186b98
Christian Rémillard <christian.remillard@gmail.com>
2024-05-08 10:49:32 -0400

    Build v$npm_package_version

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,10 +1,10 @@
 /**
  * Librairie du system desing des Bibliothèques de l'Université de Montréal
  * @module @bibudem/ui
- * @version 0.5.0
+ * @version 0.6.0
  * @author Christian Rémillard <christian.remillard@umontreal.ca>
  * @license ISC
  * @see https://github.com/bibudem/ui
  */
-Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("./bib-avis.cjs");exports.BibAvis=e.BibAvis;
+Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("./bib-avis.cjs"),r=require("./udem-urgence.cjs");exports.BibAvis=e.BibAvis,exports.UdeMUrgence=r.UdeMUrgence;
 //# sourceMappingURL=index.cjs.map
--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
 /**
  * Librairie du system desing des Bibliothèques de l'Université de Montréal
  * @module @bibudem/ui
- * @version 0.5.0
+ * @version 0.6.0
  * @author Christian Rémillard <christian.remillard@umontreal.ca>
  * @license ISC
  * @see https://github.com/bibudem/ui
  */
 import { BibAvis } from "./bib-avis.js";
+import { UdeMUrgence } from "./udem-urgence.js";
 export {
-  BibAvis
+  BibAvis,
+  UdeMUrgence
 };
 //# sourceMappingURL=index.js.map
