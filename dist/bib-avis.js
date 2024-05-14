e6e7eaef7767830ff6b48ed2f13797016a1261cf
Christian Rémillard <christian.remillard@gmail.com>
2024-05-14 16:50:12 -0400

    chore: release 0.8.0
    Release-As: 0.8.8

--- dist/bib-avis.js
+++ dist/bib-avis.js
@@ -1,7 +1,7 @@
 /**
  * Librairie du system desing des Bibliothèques de l'Université de Montréal
  * @module @bibudem/ui
- * @version 0.7.0
+ * @version 0.8.0
  * @author Christian Rémillard <christian.remillard@umontreal.ca>
  * @license ISC
  * @see https://github.com/bibudem/ui
@@ -48,19 +48,19 @@ let h = { get(e2, t2, n2) {
     if ("store" === t2)
       return n2.objectStoreNames[1] ? void 0 : n2.objectStore(n2.objectStoreNames[0]);
   }
-  return g(e2[t2]);
+  return p(e2[t2]);
 }, set: (e2, t2, n2) => (e2[t2] = n2, true), has: (e2, t2) => e2 instanceof IDBTransaction && ("done" === t2 || "store" === t2) || t2 in e2 };
 function b(e2) {
   h = e2(h);
 }
-function g(e2) {
+function p(e2) {
   if (e2 instanceof IDBRequest)
     return function(e3) {
       const t3 = new Promise((t4, n2) => {
         const s2 = () => {
           e3.removeEventListener("success", r2), e3.removeEventListener("error", i2);
         }, r2 = () => {
-          t4(g(e3.result)), s2();
+          t4(p(e3.result)), s2();
         }, i2 = () => {
           n2(e3.error), s2();
         };
@@ -72,9 +72,9 @@ function g(e2) {
     return d.get(e2);
   const t2 = function(e3) {
     return "function" == typeof e3 ? (t3 = e3, (c || (c = [IDBCursor.prototype.advance, IDBCursor.prototype.continue, IDBCursor.prototype.continuePrimaryKey])).includes(t3) ? function(...e4) {
-      return t3.apply(p(this), e4), g(this.request);
+      return t3.apply(g(this), e4), p(this.request);
     } : function(...e4) {
-      return g(t3.apply(p(this), e4));
+      return p(t3.apply(g(this), e4));
     }) : (e3 instanceof IDBTransaction && function(e4) {
       if (l.has(e4))
         return;
@@ -94,7 +94,7 @@ function g(e2) {
   }(e2);
   return t2 !== e2 && (d.set(e2, t2), u.set(t2, e2)), t2;
 }
-const p = (e2) => u.get(e2), f = ["get", "getKey", "getAll", "getAllKeys", "count"], v = ["put", "add", "delete", "clear"], m = /* @__PURE__ */ new Map();
+const g = (e2) => u.get(e2), f = ["get", "getKey", "getAll", "getAllKeys", "count"], v = ["put", "add", "delete", "clear"], m = /* @__PURE__ */ new Map();
 function w(e2, t2) {
   if (!(e2 instanceof IDBDatabase) || t2 in e2 || "string" != typeof t2)
     return;
@@ -124,18 +124,19 @@ async function* I(...e2) {
   if (t2 instanceof IDBCursor || (t2 = await t2.openCursor(...e2)), !t2)
     return;
   const n2 = new Proxy(t2, D);
-  for (B.set(n2, t2), u.set(n2, p(t2)); t2; )
+  for (B.set(n2, t2), u.set(n2, g(t2)); t2; )
     yield n2, t2 = await (k.get(n2) || t2.continue()), k.delete(n2);
 }
-function L(e2, t2) {
+function E(e2, t2) {
   return t2 === Symbol.asyncIterator && o(e2, [IDBIndex, IDBObjectStore, IDBCursor]) || "iterate" === t2 && o(e2, [IDBIndex, IDBObjectStore]);
 }
-async function E(e2) {
+b((e2) => ({ ...e2, get: (t2, n2, s2) => E(t2, n2) ? I : e2.get(t2, n2, s2), has: (t2, n2) => E(t2, n2) || e2.has(t2, n2) }));
+const L = "avis";
+async function j(e2) {
   const t2 = new TextEncoder().encode(JSON.stringify(e2)), n2 = await crypto.subtle.digest("SHA-256", t2);
   return Array.from(new Uint8Array(n2)).map((e3) => e3.toString(16).padStart(2, "0")).join("");
 }
-b((e2) => ({ ...e2, get: (t2, n2, s2) => L(t2, n2) ? I : e2.get(t2, n2, s2), has: (t2, n2) => L(t2, n2) || e2.has(t2, n2) }));
-class j extends t {
+class S extends t {
   constructor() {
     super();
     __privateAdd(this, _n);
@@ -196,43 +197,45 @@ s_fn = async function(e2) {
   if (!("indexedDB" in window))
     return void this.setMessage(e2.message);
   const t2 = __privateSet(this, _t, await function(e3, t3, { blocked: n2, upgrade: s2, blocking: r2, terminated: i2 } = {}) {
-    const o2 = indexedDB.open(e3, t3), a2 = g(o2);
+    const o2 = indexedDB.open(e3, t3), a2 = p(o2);
     return s2 && o2.addEventListener("upgradeneeded", (e4) => {
-      s2(g(o2.result), e4.oldVersion, e4.newVersion, g(o2.transaction), e4);
+      s2(p(o2.result), e4.oldVersion, e4.newVersion, p(o2.transaction), e4);
     }), n2 && o2.addEventListener("blocked", (e4) => n2(e4.oldVersion, e4.newVersion, e4)), a2.then((e4) => {
       i2 && e4.addEventListener("close", () => i2()), r2 && e4.addEventListener("versionchange", (e5) => r2(e5.oldVersion, e5.newVersion, e5));
     }).catch(() => {
     }), a2;
   }("@bibudem/ui", 1, { upgrade(e3) {
-    e3.objectStoreNames.contains("avis") || e3.createObjectStore("avis");
+    e3.objectStoreNames.contains(L) || e3.createObjectStore(L);
   } }));
   try {
-    const n2 = await E(e2), s2 = await t2.get("avis", n2);
-    s2 ? s2.hidden || (await t2.delete("avis", n2), __privateMethod(this, _r, r_fn).call(this, s2)) : __privateMethod(this, _r, r_fn).call(this, e2);
+    const n2 = await j(e2), s2 = await t2.get(L, n2);
+    s2 ? s2.hidden || (await t2.delete(L, n2), __privateMethod(this, _r, r_fn).call(this, s2)) : __privateMethod(this, _r, r_fn).call(this, e2);
   } catch (t3) {
     console.error("Something went wrong with indexedDB: %o", t3), this.setMessage(e2.message);
   }
 };
 _r = new WeakSet();
 r_fn = async function(e2) {
-  if (this.setMessage(e2), __privateGet(this, _t)) {
-    const t2 = await E(e2);
-    await __privateGet(this, _t).add("avis", { ...e2, hidden: false }, t2);
+  if (this.dispatchEvent(new CustomEvent("bib:show", { bubbles: true, cancelable: true })) && (this.setMessage(e2), __privateGet(this, _t))) {
+    const t2 = await j(e2);
+    await __privateGet(this, _t).put(L, { ...e2, hidden: false }, t2);
   }
 };
 _i = new WeakSet();
 i_fn = async function() {
-  const e2 = await E(__privateGet(this, _e));
-  await __privateGet(this, _t).put("avis", { ...__privateGet(this, _e), hidden: true }, e2), __privateSet(this, _e, null), this.requestUpdate();
+  if (!this.dispatchEvent(new CustomEvent("bib:hide", { bubbles: true, cancelable: true })))
+    return;
+  const e2 = await j(__privateGet(this, _e));
+  await __privateGet(this, _t).put(L, { ...__privateGet(this, _e), hidden: true }, e2), __privateSet(this, _e, null), this.requestUpdate();
 };
 _o = new WeakSet();
 o_fn = function() {
   __privateMethod(this, _i, i_fn).call(this);
 };
-__publicField(j, "properties", { service: { type: String }, contexte: { type: String, default: "site-web" }, niveau: { type: String }, boutonFermer: { type: Boolean, attribute: "bouton-fermer" }, message: { state: true } });
-__publicField(j, "styles", [n`${s(':host,*{box-sizing:border-box}:host{display:block;font-size:var(--bib-avis-size, var(--md-sys-typescale-title-medium-size, inherit));background:var(--bib-avis-container-color, var(--md-sys-color-warningContainer, #fffac6))}:host([hidden]){display:none}.inner{display:flex;align-items:center;margin:0 auto;padding:11px 19px;gap:1em}:host(:not([fluide])) .inner{max-width:1220px}.message{flex-grow:1;min-height:24px}.btn-close{display:inline-flex;-webkit-box-align:center;align-items:center;-webkit-box-pack:center;justify-content:center;box-sizing:border-box;-webkit-tap-highlight-color:transparent;background-color:transparent;outline:0px;border:0px;margin:0;cursor:pointer;user-select:none;vertical-align:middle;appearance:none;text-decoration:none;text-align:center;flex:0 0 auto;font-size:1.5rem;font-size:36px;font-weight:700;line-height:1;position:relative;padding:0;border-radius:50%;overflow:visible;color:var(--bib-btn-close-color, rgba(0, 0, 0, .4));transition:color .15s cubic-bezier(.4,0,.2,1),background-color .15s cubic-bezier(.4,0,.2,1)}.btn-close:after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);min-height:44px;min-width:44px;width:100%;height:100%}.btn-close:focus:not([disabled]),.btn-close:focus-visible{outline:2px solid #bde4ff;outline-offset:3px}.btn-close:focus:not(:focus-visible){outline:0}.btn-close:hover{color:var(--bib-btn-close-hover-color, rgba(0, 0, 0, .8))}.btn-close:hover:after{background-color:#0000000a}.btn-close:after{width:calc(100% + 16px);height:calc(100% + 16px);border-radius:50%;background-color:transparent;transition:background-color .15s cubic-bezier(.4,0,.2,1) 0ms}.btn-close>svg{fill:currentColor}')}`, n``]);
-customElements.define("bib-avis", j);
+__publicField(S, "properties", { service: { type: String }, contexte: { type: String, default: "site-web" }, niveau: { type: String }, boutonFermer: { type: Boolean, attribute: "bouton-fermer" }, message: { state: true } });
+__publicField(S, "styles", [n`${s(':host,*{box-sizing:border-box}:host{display:block;font-size:var(--bib-avis-size, var(--md-sys-typescale-title-medium-size, inherit));background:var(--bib-avis-container-color, var(--md-sys-color-warningContainer, #fffac6))}:host([hidden]){display:none}.inner{display:flex;align-items:center;margin:0 auto;padding:11px 19px;gap:1em}:host(:not([fluide])) .inner{max-width:1220px}.message{flex-grow:1;min-height:24px}.btn-close{display:inline-flex;-webkit-box-align:center;align-items:center;-webkit-box-pack:center;justify-content:center;box-sizing:border-box;-webkit-tap-highlight-color:transparent;background-color:transparent;outline:0px;border:0px;margin:0;cursor:pointer;user-select:none;vertical-align:middle;appearance:none;text-decoration:none;text-align:center;flex:0 0 auto;font-size:1.5rem;font-size:36px;font-weight:700;line-height:1;position:relative;padding:0;border-radius:50%;overflow:visible;color:var(--bib-btn-close-color, rgba(0, 0, 0, .4));transition:color .15s cubic-bezier(.4,0,.2,1),background-color .15s cubic-bezier(.4,0,.2,1)}.btn-close:after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);min-height:44px;min-width:44px;width:100%;height:100%}.btn-close:focus:not([disabled]),.btn-close:focus-visible{outline:2px solid #bde4ff;outline-offset:3px}.btn-close:focus:not(:focus-visible){outline:0}.btn-close:hover{color:var(--bib-btn-close-hover-color, rgba(0, 0, 0, .8))}.btn-close:hover:after{background-color:#0000000a}.btn-close:after{width:calc(100% + 16px);height:calc(100% + 16px);border-radius:50%;background-color:transparent;transition:background-color .15s cubic-bezier(.4,0,.2,1) 0ms}.btn-close>svg{fill:currentColor}')}`, n``]);
+customElements.define("bib-avis", S);
 export {
-  j as BibAvis
+  S as BibAvis
 };
 //# sourceMappingURL=bib-avis.js.map
