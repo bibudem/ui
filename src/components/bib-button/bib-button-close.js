929eea5ce600593089e9ac7870437296c1587ca6
Christian Rémillard <christian.remillard@gmail.com>
2024-06-10 11:45:35 -0400

    feat(bib-button): Nouveau composant bib-button-close
    Émet l'événement 'bib:close' lorsque cliqué ou activé par clavier.

---
+++ src/components/bib-button/bib-button-close.js
@@ -0,0 +1,27 @@
+import { LitElement, html } from 'lit'
+import { unsafeHTML } from 'lit/directives/unsafe-html.js'
+import '@material/web/iconbutton/icon-button.js'
+import closeIcon from '../../icons/close_24dp_FILL0_wght400_GRAD0_opsz24.svg?raw'
+
+export class BibButtonClose extends LitElement {
+
+  constructor() {
+    super()
+    this.addEventListener('click', this.handleClick)
+  }
+
+  handleClick() {
+    const closeEvent = new MouseEvent('bib:close', { bubbles: true })
+    this.dispatchEvent(closeEvent)
+  }
+
+  render() {
+    return html`
+      <md-icon-button aria-label="fermer">
+        ${unsafeHTML(closeIcon)}
+      </md-icon-button>
+    `
+  }
+}
+
+customElements.define('bib-button-close', BibButtonClose)
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export * from './components/bib-avis/bib-avis.js'
 export * from './components/udem-urgence/udem-urgence.js'
+export * from './components/bib-button/bib-button-close.js'
