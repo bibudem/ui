3d5170234f90d7cba6e4201090588299bc0d9127
Christian Rémillard <christian.remillard@umontreal.ca>
2026-03-30 14:48:00 -0400

    chore:Build pour v1.3.8
    Release-As: 1.3.8

--- dist/bib-gtm.js
+++ dist/bib-gtm.js
@@ -13,7 +13,6 @@ var __typeError = (msg) => {
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
 var __accessCheck = (obj, member, msg) => member.has(obj) || __typeError("Cannot " + msg);
-var __privateGet = (obj, member, getter) => (__accessCheck(obj, member, "read from private field"), getter ? getter.call(obj) : member.get(obj));
 var __privateAdd = (obj, member, value) => member.has(obj) ? __typeError("Cannot add the same private member more than once") : member instanceof WeakSet ? member.add(obj) : member.set(obj, value);
 var __privateMethod = (obj, member, method) => (__accessCheck(obj, member, "access private method"), method);
 var _t, _g_instances, e_fn;
@@ -35,40 +34,39 @@ class g extends t {
 _t = new WeakMap();
 _g_instances = new WeakSet();
 e_fn = function() {
-  const t2 = this;
   console.log("[bib-gtm] Initializing...");
-  const e2 = this.containerId;
+  const t2 = this.containerId;
   setTimeout(() => {
-    const n2 = document.querySelector("bib-consent");
-    if (n2) {
-      let o2 = function(t3) {
-        console.log(`[bib-gtm] <bib-consent> triggered event type ${t3.type} with data:`, t3.detail);
-        const n3 = t3.detail;
+    const e2 = document.querySelector("bib-consent");
+    if (e2) {
+      let n2 = function(e3) {
+        console.log(`[bib-gtm] <bib-consent> triggered event type ${e3.type} with data:`, e3.detail);
+        const n3 = e3.detail;
         if (null !== n3) {
-          console.log("[bib-gtm] Loading GTM script."), function(t5) {
+          console.log("[bib-gtm] Loading GTM script."), function(t3) {
             if (!c) {
-              c = document.createElement("script"), c.async = true, c.src = `https://www.googletagmanager.com/gtm.js?id=${t5}`;
-              var e3 = document.getElementsByTagName("script")[0];
-              e3.parentNode.insertBefore(c, e3);
+              c = document.createElement("script"), c.async = true, c.src = `https://www.googletagmanager.com/gtm.js?id=${t3}`;
+              var e5 = document.getElementsByTagName("script")[0];
+              e5.parentNode.insertBefore(c, e5);
             }
-          }(e2);
-          const { analytics_consent: t4, ad_consent: o3 } = n3, i3 = { ad_user_data: o3, ad_personalization: o3, ad_storage: o3, analytics_storage: t4 };
-          let s2, l2;
-          console.log("[bib-gtm] Updating GTM with consent data:", i3), a2("consent", "update", i3);
+          }(t2);
+          const { analytics_consent: e4, ad_consent: o3 } = n3, s2 = { ad_user_data: o3, ad_personalization: o3, ad_storage: o3, analytics_storage: e4 };
+          let a3, l2;
+          console.log("[bib-gtm] Updating GTM with consent data:", s2), i2("consent", "update", s2);
           try {
-            s2 = window.top.location.href, l2 = window.top.location.pathname;
-          } catch (t5) {
-            s2 = window.location.href, l2 = window.location.pathname;
+            a3 = window.top.location.href, l2 = window.top.location.pathname;
+          } catch (t3) {
+            a3 = window.location.href, l2 = window.location.pathname;
           }
-          console.log("[bib-gtm] Sending page_view with location:", s2, "path:", l2, "title:", document.title), a2("event", "page_view", { page_path: l2, page_location: s2, page_title: document.title });
+          console.log("[bib-gtm] Sending page_view with location:", a3, "path:", l2, "title:", document.title), i2("event", "page_view", { page_path: l2, page_location: a3, page_title: document.title });
         }
       };
       console.log("[bib-gtm] bib-consent element found.");
-      const i2 = globalThis.dataLayer = globalThis.dataLayer || [], a2 = globalThis.gtag = globalThis.gtag || function() {
-        i2.push(arguments);
-      }, g2 = new s(false);
-      console.log("[bib-gtm] Pushing default consent to GTM with `defaultConsent`: ", g2), a2("consent", "default", g2.toGTM()), i2.push({ "gtm.start": (/* @__PURE__ */ new Date()).getTime(), event: "gtm.js" }), console.log("[bib-gtm] Registering event listeners on <bib-consent> element."), n2.addEventListener(l.READY, o2), n2.addEventListener(l.CHANGE, o2);
-    } else __privateGet(t2, _t).warn("No bib-consent element found");
+      const o2 = globalThis.dataLayer = globalThis.dataLayer || [], i2 = globalThis.gtag = globalThis.gtag || function() {
+        o2.push(arguments);
+      }, a2 = new s(false);
+      console.log("[bib-gtm] Pushing default consent to GTM with `defaultConsent`: ", a2), i2("consent", "default", a2.toGTM()), o2.push({ "gtm.start": (/* @__PURE__ */ new Date()).getTime(), event: "gtm.js" }), console.log("[bib-gtm] Registering event listeners on <bib-consent> element."), e2.addEventListener(l.READY, n2), e2.addEventListener(l.CHANGE, n2);
+    } else console.log("[bib-gtm] No bib-consent element found");
     console.log("[bib-gtm] Initialization complete.");
   });
 };
